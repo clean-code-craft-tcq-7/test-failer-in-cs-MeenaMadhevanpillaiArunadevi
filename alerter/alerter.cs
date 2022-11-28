c2e5761170bd975ea71cf6264c06268f16ecdbc1
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-11-28 14:36:08 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -26,6 +26,7 @@ namespace AlerterSpace {
             alertInCelcius(400.5f);
             alertInCelcius(303.6f);
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
+            Debug.Assert(alertFailureCount == 0);
             Console.WriteLine("All is well (maybe!)\n");
         }
     }
