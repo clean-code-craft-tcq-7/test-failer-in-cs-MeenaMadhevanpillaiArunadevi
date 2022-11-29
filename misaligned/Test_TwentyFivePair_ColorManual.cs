554bc3a664f63b91304eab0d96ef2ed58400532b
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-11-29 19:13:15 +0530

    Update Test_TwentyFivePair_ColorManual.cs

--- misaligned/Test_TwentyFivePair_ColorManual.cs
+++ misaligned/Test_TwentyFivePair_ColorManual.cs
@@ -1,5 +1,13 @@
 using System;
-            int result = printColorMap();
-            Debug.Assert(result == 25);
-            Debug.Assert(result == 36);
-            Console.WriteLine("All is well (maybe!)");
+namespace MisalignedSpace{
+    public partial class Misaligned{
+            public static void testTwentyFivePair_ColorManual(){
+                        int result = printColorMap();
+                        int result = printColorMap();
+                        Debug.Assert(result == 25);
+                        Debug.Assert(result == 36);
+                        Console.WriteLine("All is well (maybe!)");
+            }
+    }
+}
+           
