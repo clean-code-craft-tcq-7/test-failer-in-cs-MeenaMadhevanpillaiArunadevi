646d48b0b642b447d7cb1e477cfcb67d5aa1108c
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-11-29 18:59:41 +0530

    Update testAlerter.cs

--- alerter/testAlerter.cs
+++ alerter/testAlerter.cs
@@ -1,1 +1,12 @@
-
+using System;
+namespace AlerterSpace{
+  public partial class Alerter{
+      public static void TestAlerter(){
+            alertInCelcius(400.5f);
+            alertInCelcius(303.6f);
+            Console.WriteLine("{0} alerts failed.", alertFailureCount);
+            Debug.Assert(alertFailureCount == 0);
+            Console.WriteLine("All is well (maybe!)\n");
+      }
+  }
+}
