5f567c8676921c87c4f5ecb669a34f82f2494f56
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-01 23:02:12 +0530

    Update Test_TshirtSizeCheck.cs

--- tshirts/Test_TshirtSizeCheck.cs
+++ tshirts/Test_TshirtSizeCheck.cs
@@ -9,6 +9,8 @@ namespace TshirtSpace {
             Debug.Assert(Size(37) == "S");
             Debug.Assert(Size(40) == "M");
             Debug.Assert(Size(43) == "L");
+            Debug.Assert(Size(38) == "S");
+            Debug.Assert(Size(42) == "M");
             Debug.Assert(Size(-1) == "Not exists");
             Console.WriteLine( "All is well (maybe!)");
       
