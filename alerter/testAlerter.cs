0e9b5e03e48d20ea3deeae71d1e79b1008d720a1
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-01 22:56:41 +0530

    Update testAlerter.cs

--- alerter/testAlerter.cs
+++ alerter/testAlerter.cs
@@ -3,8 +3,9 @@ using System.Diagnostics;
 namespace AlerterSpace{
   public partial class Alerter{
       public static void TestAlerter(){
-            alertInCelcius(400.5f);
-            alertInCelcius(303.6f);
+            alertInCelcius(400.5f); //>392F (200C)
+            alertInCelcius(392.0f); //=392F
+            alertInCelcius(303.6f); //<392F
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Debug.Assert(alertFailureCount == 0);
             Console.WriteLine("All is well (maybe!)\n");
