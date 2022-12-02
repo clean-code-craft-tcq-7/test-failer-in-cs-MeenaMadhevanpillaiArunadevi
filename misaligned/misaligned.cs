d6816da36efe39817951efa3677f738be289b3a4
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-02 18:34:54 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -3,16 +3,22 @@ using System.Diagnostics;
 
 namespace MisalignedSpace {
     public partial class Misaligned {
-        static int printColorMap() {
+        static string printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int i = 0, j = 0;
+            string result = "";
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
+                    if(number == (i * 5 + j))
+                    {
+                        result = String.Format("{0} | {1}", majorColors[i], minorColors[i]);
+                    }
+					
                     Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
-            return i * j;
+            return result;
         }
         static void Main(string[] args) {
            testTwentyFivePair_ColorManual();
