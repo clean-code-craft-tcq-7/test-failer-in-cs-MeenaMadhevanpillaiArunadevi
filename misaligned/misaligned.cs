cf20d1e4c6949f6cc3b4c6df30b992f4d941f079
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-03 22:45:52 +0530

    Update misaligned.cs
    Pass the failed tests

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,27 +1,40 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
-namespace MisalignedSpace {
-    public partial class Misaligned {
-        static string printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+namespace MisalignedSpace
+{
+    public partial class Misaligned
+    {
+        static int printColorMap()
+        {
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
             int i = 0, j = 0;
-            string result = "";
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    if(number == (i * 5 + j))
-                    {
-                        result = String.Format("{0} | {1}", majorColors[i], minorColors[i]);
-                    }
-					
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    Console.WriteLine(formatColorPairFromColorIndex(i, j));
                 }
             }
-            return result;
+            return i * j;
         }
-        static void Main(string[] args) {
-           testTwentyFivePair_ColorManual();
+        public static string formatColorPairFromColorIndex(int i, int j)
+        {
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+            string colorpair;
+            if (i * 5 + j + 1 < 10)
+            {
+                colorpair = String.Format("{0}  | {1} | {2}", i * 5 + j + 1, majorColors[i], minorColors[j]);
+            }
+            else
+            {
+                colorpair = String.Format("{0} | {1} | {2}", i * 5 + j + 1, majorColors[i], minorColors[j]);
+            }
+            return colorpair;
         }
-    }
-}
+        static void Main(string[] args)
+        {
+            test_ColorPair();
+        }
