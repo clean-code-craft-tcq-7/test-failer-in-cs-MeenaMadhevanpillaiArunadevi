df168f6dad5ef5ba87f6d4899be2dd3a890161b5
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-03 22:49:40 +0530

    Update testAlerter.cs

--- alerter/testAlerter.cs
+++ alerter/testAlerter.cs
@@ -2,13 +2,37 @@ using System;
 using System.Diagnostics;
 namespace AlerterSpace{
   public partial class Alerter{
-      public static void TestAlerter(){
-            alertInCelcius(400.5f); //>392F (200C)
-            alertInCelcius(392.0f); //=392F
-            alertInCelcius(303.6f); //<392F
-            Console.WriteLine("{0} alerts failed.", alertFailureCount);
-            Debug.Assert(alertFailureCount == 0);
+       public static void TestAlerter(string env)
+        {
+            switch (env)
+            {
+                case "dev":
+                    alertInCelcius(networkAlertStub, 392f);
+                    Console.WriteLine("{0} alerts failed.", alertFailureCount);
+                    Debug.Assert(alertFailureCount == 0);
+
+                    alertInCelcius(networkAlertStub, 303.6f);
+                    Console.WriteLine("{0} alerts failed.", alertFailureCount);
+                    Debug.Assert(alertFailureCount == 0);
+
+                    alertInCelcius(networkAlertStub, 400.5f);
+                    Console.WriteLine("{0} alerts failed.", alertFailureCount);
+                    Debug.Assert(alertFailureCount == 1);
+                    break;
+                case "prod":
+                    alertInCelcius(networkAlert, 392f);
+                    Debug.Assert(alertFailureCount == 0);
+                    alertInCelcius(networkAlert, 303.6f);
+                    Debug.Assert(alertFailureCount == 0);
+                    alertInCelcius(networkAlert, 400.5f);
+                    Debug.Assert(alertFailureCount == 0);
+                    break;
+                default:
+                    throw new Exception("Environment setting in valid");
+            }
+
             Console.WriteLine("All is well (maybe!)\n");
-      }
+
+        }
   }
 }
