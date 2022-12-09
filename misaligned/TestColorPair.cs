0e70c6aae558c5b4bca511bbdbd058441c2aecc5
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-09 16:35:13 +0530

    Included test for variable color length

--- misaligned/TestColorPair.cs
+++ misaligned/TestColorPair.cs
@@ -7,13 +7,13 @@ namespace MisalignedSpace{
             int result = printColorMap();
             Debug.Assert(result == 25);
             //Test if Color Pair number starts from 1 instead of 0 (i.e., 1-25 color pair)
-            Debug.Assert(formatColorPairFromColorIndex(0, 0) == "1  | White | Blue");
+            Debug.Assert(formatColorPairFromColorIndex(0, 0) == "1  | White  | Blue");
             //Test Single Digit Color Pair number
-            Debug.Assert(formatColorPairFromColorIndex(0, 4) == "5  | White | Slate");
+            Debug.Assert(formatColorPairFromColorIndex(0, 4) == "5  | White  | Slate");
             //Test Double Digit Color Pair number
-            Debug.Assert(formatColorPairFromColorIndex(2, 3) == "14 | Black | Brown");
+            Debug.Assert(formatColorPairFromColorIndex(2, 3) == "14 | Black  | Brown");
             //Test variable length Colors
-            Debug.Assert(formatColorPairFromColorIndex(1, 1) == "7  | Red   | Orange");
+            Debug.Assert(formatColorPairFromColorIndex(1, 1) == "7  | Red    | Orange");
             Console.WriteLine("All is well (maybe!)");
         }
     }
